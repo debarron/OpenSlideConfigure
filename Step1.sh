970686a9224f7654c9f85b5911a5e71ba06e17c0
debarron <de.lopezbarron@gmail.com>
2016-10-10 12:56:01 -0500

    Adding the first script to configuree OpenSlide and the OpenSlide-Java repo

--- Step1.sh
+++ Step1.sh
@@ -42,11 +42,19 @@
 
 # Run it in cloudlab
 
+# NEEDS ANT
+# Comment line two of /etc/apt/sources.list
+# Update the system
+
+echo ">>> Editing the sources.list file" 
+sudo sed -i -e '2s/^/#/' /etc/apt/sources.list
+
 echo ">>> Updating the system"
 sudo apt-get -y update  
 
 echo ">>> Installing dependencies"
-sudo apt-get -y install libjpeg-dev libpng12-dev liblcms2-dev  libtiff-dev libpng-dev libz-dev libopenjpeg-dev libopenjpeg5 libopenjpeg5-dbg openjpeg-tools libcairo2-dev libglib2.0-dev libgtk2.0-dev libxml2-dev sqlite3 libsqlite3-dev libopenblas-base openslide-tools  pkg-config 
+sudo apt-get -y install libjpeg-dev libpng12-dev liblcms2-dev libtiff-dev libpng-dev libz-dev libopenjpeg-dev libopenjpeg5 libopenjpeg5-dbg openjpeg-tools libcairo2-dev libglib2.0-dev libgtk2.0-dev libxml2-dev sqlite3 libsqlite3-dev libopenblas-base openslide-tools pkg-config python-software-properties ant
+
 
 echo ">>> Updating system variables"
 export LD_LIBRARY_PATH=/usr/lib/openblas-base/
@@ -56,6 +64,13 @@ git clone https://github.com/openslide/openslide-java.git
 
 echo ">>> Installing OpenSlide for Java"
 cd "./openslide-java"
+libtoolize --force
+aclocal
+autoheader
+automake --force-missing --add-missing
+autoconf
+
+
 ./configure
 make
 make install
